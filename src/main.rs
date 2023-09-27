febacdc7b7bde686671cc5f938030b9fb67d3b0c
known <mondo.beyondo3@gmail.com>
2023-09-26 21:56:59 -0400

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+// whatodo add "Make that one function"
+// whatodo checkout all | Prints all todos
+// whatodo checkout done | Prints all todos marked done
+// whatodo checkout todo | Prints all todos not marked done
+// whatodo complete 1 | Marks first todo as complete, will use 1 indexed list
+// whatodo delete 1 | Deletes first todo, will use 1 indexed list
+// whatodo new list | Creates new list in current directory
+
+fn main() {
+    println!("Hello, world!");
+}
