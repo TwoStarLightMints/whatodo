315ca35b5d5af147d545487ae59dd8ac67e75c4d
known <mondo.beyondo3@gmail.com>
2023-09-27 21:20:45 -0400

    Begin abstracting fundamental program methods to functions

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,8 @@
 // whatodo new list | Creates new list in current directory
 
 use std::{
-    fs,
-    io::Read,
+    fs::File,
+    io::{Error, Read, Write},
     {env, fmt},
 };
 
@@ -18,15 +18,15 @@ struct Todo {
 }
 
 impl Todo {
-    fn new(contents: String) -> Self {
+    fn new(complete: Option<bool>, contents: String) -> Self {
+        // Takes an option to allow for loading from file
         Self {
-            complete: false,
+            complete: match complete {
+                Some(b) => b,
+                None => false,
+            },
             contents,
         }
-    }
-
-    fn load(complete: bool, contents: String) -> Self {
-        Self { complete, contents }
     }
 
     fn complete(&mut self) {
@@ -44,45 +44,80 @@ impl fmt::Display for Todo {
     }
 }
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    let args_clipped = &args[1..];
+fn load_todos(todo_string: String) -> Vec<Todo> {
+    // Loads todos read in from file
+    todo_string
+        .split('\n') // All todos will be separated by a newline character
+        .filter(|e| !e.is_empty()) // Get all non-empty todos from todos.txt
+        .map(|e| e.split('|')) // The separator from complete and contents is the pipe character
+        .map(|e| {
+            let e_strs: Vec<&str> = e.collect();
+            Todo::new(Some(e_strs[0] == "1"), String::from(e_strs[1].trim()))
+        })
+        .collect()
+}
 
-    let mut tf = fs::File::open("todos.txt").unwrap();
+fn read_todos() -> Result<String, Error> {
+    // Called by load_todos
+    // Open todo file for retrieving previous todos
+    let mut itf = File::open(format!(
+        "{}{}",
+        env::current_dir().unwrap().to_str().unwrap(),
+        "todos.txt"
+    ))?;
     let mut buf = String::new();
 
-    tf.read_to_string(&mut buf);
+    itf.read_to_string(&mut buf)?;
 
-    let todos_list: Vec<&str> = buf
-        .split('\n')
-        .filter(|e| !e.is_empty())
-        .map(|e| e.trim())
-        .collect();
+    Ok(buf)
+}
 
-    let final_todos: Vec<Vec<&str>> = todos_list
-        .iter()
-        .map(|e| e.split(' ').collect::<Vec<&str>>())
-        .collect();
+fn main_loop(args: &[String]) {
+    let todos_list = load_todos(read_todos().unwrap());
+}
 
-    let mutated_todos: Vec<Todo> = final_todos
-        .iter()
-        .map(|e| Todo::load(e[0] == "1", String::from(e[1])))
-        .collect();
+fn main() {
+    let env_args: Vec<String> = env::args().collect();
+    let args = &env_args[1..]; // Get arguments and collect all necessary for operation of the program
 
-    if args_clipped[0] == "checkout" {
-        if args_clipped[1] == "all" {
-            for todo in mutated_todos {
+    let mut itf = File::open("todos.txt").unwrap(); // Open the todo file for processing
+    let mut buf = String::new();
+
+    itf.read_to_string(&mut buf).unwrap(); // Get all of the todos from the file
+
+    let todos_list: Vec<Todo> = load_todos(buf);
+
+    if args[0] == "checkout" {
+        if args[1] == "all" {
+            for todo in &todos_list {
                 println!("{}", todo);
             }
-        } else if args_clipped[1] == "done" {
-            for todo in mutated_todos.iter().filter(|e| e.complete) {
+        } else if args[1] == "done" {
+            for todo in todos_list.iter().filter(|e| e.complete) {
                 println!("{}", todo);
             }
-        } else if args_clipped[1] == "todo" {
-            for todo in mutated_todos.iter().filter(|e| !e.complete) {
+        } else if args[1] == "todo" {
+            for todo in todos_list.iter().filter(|e| !e.complete) {
                 println!("{}", todo);
             }
         }
+    }
+
+    let mut otf = File::create("todos.txt").unwrap();
+
+    for todo in todos_list {
+        otf.write(
+            format!(
+                "{}|{}\n",
+                match todo.complete {
+                    true => "1",
+                    false => "0",
+                },
+                todo.contents
+            )
+            .as_bytes(),
+        )
+        .unwrap();
     }
 
     // let mut stuff = env::current_dir().unwrap();
