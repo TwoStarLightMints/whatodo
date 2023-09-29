f03ae36f770711e498941cfb55cfb7df34d4f8dd
known <mondo.beyondo3@gmail.com>
2023-09-29 00:02:53 -0400

    Add help display

--- src/main.rs
+++ src/main.rs
@@ -144,42 +144,69 @@ fn save_todos(todos_list: Vec<Todo>) {
     }
 }
 
+fn help() {
+    println!("usage: whatodo <command> [<args>]");
+    println!();
+    println!("\tCreate a new todo list in current working directory:");
+    println!();
+    println!("\t\twhatodo init");
+    println!();
+    println!("\tAdd item to todo list in current working directory:");
+    println!();
+    println!("\t\twhatodo add 'Description of todo item here'");
+    println!();
+    println!("\tComplete an item on todo list:");
+    println!();
+    println!("\t\twhatodo complete (number of the item you would like to complete)");
+    println!();
+    println!("\tRemove an item from the todo list:");
+    println!();
+    println!("\t\twhatodo remove (number of the item you would like to complete)");
+    println!();
+    println!("\tDisplay items in todo list:");
+    println!();
+    println!("\t\twhatodo checkout (all|done|todo)");
+}
+
 fn main() {
     let env_args: Vec<String> = env::args().collect();
     let args = &env_args[1..]; // Get arguments and collect all necessary for operation of the program
 
-    if args.len() == 0 {
-        println!("Display help");
-    } else {
-        if args[0] == "init" {
-            // If user has not created a todo, create it
-            init_new_list().unwrap();
-        } else {
-            let mut todos_list: Vec<Todo> = load_todos();
+    // Use if let statements to check the length of the arguments sent in and give them descriptive values
+    if let [command, value] = args {
+        let mut todos_list: Vec<Todo> = load_todos();
 
-            if args[0] == "checkout" {
-                checkout_list(args[1].as_str(), &todos_list);
-            } else if args[0] == "add" {
-                // This branch involves all functionality for adding todos
-                todos_list.push(Todo::new(None, String::from(args[1].clone())));
-            } else if args[0] == "complete" {
-                // This branch involves all functionality with completing todos
-                match args[1].parse::<usize>() {
-                    Ok(num) => {
-                        todos_list[num - 1].complete = true;
-                    }
-                    Err(_) => println!("Invalid index given"),
+        if command == "checkout" {
+            checkout_list(value.as_str(), &todos_list);
+        } else if command == "add" {
+            // This branch involves all functionality for adding todos
+            todos_list.push(Todo::new(None, String::from(value.clone())));
+            save_todos(todos_list);
+        } else if command == "complete" {
+            // This branch involves all functionality with completing todos
+            match value.parse::<usize>() {
+                Ok(num) => {
+                    todos_list[num - 1].complete = true;
                 }
-            } else if args[0] == "remove" {
-                todos_list = remove_from_list(args[1].as_str(), todos_list);
+                Err(_) => println!("Invalid index given"),
             }
-
+            save_todos(todos_list);
+        } else if command == "remove" {
+            todos_list = remove_from_list(value.as_str(), todos_list);
             save_todos(todos_list);
         }
+    } else if let [command] = args {
+        if command == "init" {
+            // If user has not created a todo, create it
+            init_new_list().unwrap();
+        } else if command == "help" {
+            help();
+        }
+    } else {
+        if args.len() > 2 {
+            eprintln!("Too many arguments provided!");
+        }
+
+        help();
     }
-
-    // let mut stuff = env::current_dir().unwrap();
-    // stuff.push("todos.txt");
-
-    // println!("{:?}", stuff);
 }
