f8f4574b97964ee651eeea55bc9ee625a7008ed1
known <mondo.beyondo3@gmail.com>
2024-12-12 07:37:34 -0500

    Add initial error types

--- src/error.rs
+++ src/error.rs
@@ -1,7 +1,22 @@
+#[derive(Debug)]
 pub enum WhatodoError {
     IndexOutOfBounds,
-    CannotLoadTodos,
-    CannotInitTodos,
+    CannotLoadTodos(std::io::Error),
+    CannotInitTodos(std::io::Error),
     TodoAlreadyInList,
-    CannotSaveTodos,
+    CannotSaveTodos(std::io::Error),
 }
+
+impl std::fmt::Display for WhatodoError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::IndexOutOfBounds => write!(f, "Index out of bounds, could not perform operation"),
+            Self::CannotLoadTodos(e) => write!(f, "Could not load todos: {e}"),
+            Self::CannotInitTodos(e) => write!(f, "Could not init whatodo: {e}"),
+            Self::TodoAlreadyInList => write!(f, "Todo is already in list, could not add todo"),
+            Self::CannotSaveTodos(e) => write!(f, "Could not save todos: {e}"),
+        }
+    }
+}
+
+impl std::error::Error for WhatodoError {}
